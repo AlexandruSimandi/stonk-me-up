309353743e89e88d983e724d5be7956fa1ec4b9a
Alexandru Simandi <alex@code11.com>
2021-02-05 20:12:25 +0200

    cleanup and hotfix

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ function openStuff() {
   const symbol = document.getElementById("symbol").value
 
   const sites = [
-    "https://finance.yahoo.com/quote/<SMYBOL>",
+    "https://finance.yahoo.com/quote/<SYMBOL>",
     "https://www.marketwatch.com/investing/stock/<symbol>",
-    "https://widgets.tipranks.com/content/etoro/etoro-widget.html?lang=en&ticker=<SYMBOL>",
+    // "https://widgets.tipranks.com/content/etoro/etoro-widget.html?lang=en&ticker=<SYMBOL>",
     "https://www.tipranks.com/stocks/<symbol>/forecast",
     "https://stocktwits.com/symbol/<SYMBOL>",
     "https://finviz.com/quote.ashx?t=<SYMBOL>",
